a78abddca94aa23d489cb370449c077efa8e5136
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-02-10 11:58:29 +0530

    Home screen done

--- lib/screens/main/home/home.dart
+++ lib/screens/main/home/home.dart
@@ -24,8 +24,8 @@ class _HomeState extends State<Home> {
           fontSize: 30,
           fontWeight: FontWeight.bold,
         ),
-        actions: [
-          const CustomNetworkImage(
+        actions: const [
+          CustomNetworkImage(
             height: 45,
             width: 45,
             url:
@@ -37,35 +37,75 @@ class _HomeState extends State<Home> {
         automaticallyImplyLeading: false,
       ),
       body: Container(
-        width: size.width,
-        height: size.height,
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
-              decoration: BoxDecoration(
-                color: kwhite,
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 20.0,
-                    color: Colors.black12,
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          width: size.width,
+          height: size.height,
+          child: ListView.separated(
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return const ConversationCard();
+              },
+              separatorBuilder: (context, index) => const SizedBox(height: 5),
+              itemCount: 10)),
+    );
+  }
+}
+
+class ConversationCard extends StatelessWidget {
+  const ConversationCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+      decoration: const BoxDecoration(
+        color: kwhite,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 10),
+            blurRadius: 20.0,
+            color: Colors.black12,
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              const CircularNetworkImage(
+                height: 60,
+                width: 60,
+                url:
+                    "https://cdn.hashnode.com/res/hashnode/image/upload/v1601295799278/OsMsXdM2F.jpeg",
+              ),
+              const SizedBox(width: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  CustomText(
+                    text: "Supun Sandaruan",
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  CustomText(
+                    text: "Really, Thank you",
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
                   ),
                 ],
               ),
-              child: Row(
-                children: [
-                  CircularNetworkImage(
-                    height: 60,
-                    width: 60,
-                    url:
-                        "https://cdn.hashnode.com/res/hashnode/image/upload/v1601295799278/OsMsXdM2F.jpeg",
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+            ],
+          ),
+          const CustomText(
+            text: "1 min ago",
+            fontSize: 12,
+            color: greyColor,
+            fontWeight: FontWeight.w500,
+          ),
+        ],
       ),
     );
   }
