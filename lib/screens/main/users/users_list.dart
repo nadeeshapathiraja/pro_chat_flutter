83d19705a9546a6f3ec2ebc4463c959301aadc33
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-02-10 12:05:29 +0530

    Users page added

---
+++ lib/screens/main/users/users_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Users extends StatefulWidget {
+  const Users({Key? key}) : super(key: key);
+
+  @override
+  _UsersState createState() => _UsersState();
+}
+
+class _UsersState extends State<Users> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
